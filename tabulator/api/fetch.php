aa35ad22a4484a0c534928083d80fffcd53153a2
Krishna Bahadur Raut <106099307+rautkrishna575@users.noreply.github.com>
2025-03-21 14:22:14 +0545

    Update fetch.php

--- tabulator/api/fetch.php
+++ tabulator/api/fetch.php
@@ -1,6 +1,8 @@
 <?php
 require_once('../../db.php');
-
+header("Access-Control-Allow-Origin: *"); 
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); 
+header("Access-Control-Allow-Headers: Content-Type, Authorization"); 
 $db = new DB;
 
 if (isset($_POST['method']) && $_POST['method'] == 'fetch'):
